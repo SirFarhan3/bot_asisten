6ec2bf0377db76b44b3b752bbf22a48e41ac588a
Frai3 <farhan.raihan@student.umn.ac.id>
2025-09-03 16:22:15 +0700

    commit

--- middleware/format_rupiah.py
+++ middleware/format_rupiah.py
@@ -1,8 +1,9 @@
 import locale
 
 def format_rupiah(angka, with_prefix=True, desimal=0):
-    locale.setlocale(locale.LC_NUMERIC, 'IND')
-    rupiah = locale.format_string("%.*f", (desimal, angka), True)
-    if with_prefix:
-        return "Rp.{}".format(rupiah)
-    return rupiah
+    # locale.setlocale(locale.LC_NUMERIC, 'id_ID.UTF-8')
+    # rupiah = locale.format_string("%.*f", (desimal, angka), True)
+    # if with_prefix:
+    #     return "Rp.{}".format(rupiah)
+    # return rupiah
+    return f"Rp.{angka:,}".replace(",", ".")
